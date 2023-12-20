337ad5cdd942b277ac2680fd3efe491a25d41926
Escanor Liones <ReverseControl@users.noreply.github.com>
2023-12-20 17:21:07 -0400

    [cado-nfs/ecmongpu] Install from source.

---
+++ build.sh
@@ -0,0 +1,14 @@
+#git clone https://gitlab.inria.fr/cado-nfs/cado-nfs.git
+cd cado-nfs
+mkdir build
+cd build
+#cmake ..
+#make
+cd ../../
+git clone https://github.com/Chair-for-Security-Engineering/ecmongpu.git
+cd ecmongpu
+mkdir build
+cd build
+cmake ..
+make
+
